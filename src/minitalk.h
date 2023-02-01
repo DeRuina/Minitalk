c3166073d2262682098065c4481cacd3d68f6678
Dean Ruina <ruinadd@gmail.com>
2023-02-01 18:57:13 +0200

    added functions to h file

--- src/minitalk.h
+++ src/minitalk.h
@@ -6,15 +6,19 @@
 /*   By: druina <druina@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 13:53:51 by druina            #+#    #+#             */
-/*   Updated: 2023/01/31 14:39:30 by druina           ###   ########.fr       */
+/*   Updated: 2023/02/01 18:55:29 by druina           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-#include "../libft/src/libft.h"
-#include <signal.h>
-#include <unistd.h>
+# include "../libft/src/libft.h"
+# include <signal.h>
+# include <unistd.h>
+
+static void	handler(int signal);
+static void	send_bit_by_bit(int pid, char c);
+static char	*append_to_a_string(char *message, char c);
 
 #endif
